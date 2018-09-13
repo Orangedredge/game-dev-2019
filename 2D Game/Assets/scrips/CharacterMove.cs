035fd799b7ea2013476df4709467638a8d0a5626
Unknown <mckay.r456@stu.nebo.edu>
2018-09-13 10:29:09 -0600

    9/13/18

---
+++ 2D Game/Assets/scrips/CharacterMove.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour {
+
+
+	// player movement variables
+public float moveSpeed;
+public float jumpheight;
+
+// player grounded variables
+public Transform groundCheck;
+public float groundCheckRadius;
+public LayerMask whatIsGround;
+private bool grounded;
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+
+	
+	void FixedUpdate() {
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyDown (KeyCode.Space)&& grounded){
+			Jump();
+		}
+		//this code makes the character move from side to side using the A and D keys
+		if (Input.GetKey (KeyCode.D)){
+			GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+		if (Input.GetKey (KeyCode.A)){
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+		
+	
+
+	}
+	
+	public void Jump(){
+	GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpheight);
+	}
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# game-dev-2019
+# Game-dev
+4 me class

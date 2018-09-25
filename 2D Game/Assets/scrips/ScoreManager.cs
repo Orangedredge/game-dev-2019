58f93550801494c55c4e6c3610d5e0d1b9fefd73
Unknown <mckay.r456@stu.nebo.edu>
2018-09-25 10:29:33 -0600

    coin

--- 2D Game/Assets/scrips/ScoreManager.cs
+++ 2D Game/Assets/scrips/ScoreManager.cs
@@ -2,30 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class Scoremanager : MonoBehaviour {
 
-	public static int Score;
-	
-
-	Text ScoreText;
-
+public class ScoreManager : MonoBehaviour {
+public static int Score;
+Text ScoreText;
 	// Use this for initialization
 	void Start () {
 		ScoreText = GetComponent<Text>();
-
-		Score = 0;
+		Score = -1;
 	}
-
-
 	
 	// Update is called once per frame
 	void Update () {
-		if (Score < 0)
-			Score = 0;
-
 
 		ScoreText.text = " " + Score;
+		if (Score < 0)
+		{
+			Score = 0;
+			
+		}
+	}
 
-
+	public static void AddPoints (int PointsToAdd)
+	{
+		Score += PointsToAdd;
 	}
 }
